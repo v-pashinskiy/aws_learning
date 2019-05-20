fe5e978bf8bda40c553baa542d643246dd8c05a9
vopas <vopas@ciklum.com>
2019-05-20 11:05:11 +0300

    Init project structure

---
+++ src/main/java/com/example/aws_lerning/AwsLerningApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_lerning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLerningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLerningApplication.class, args);
+	}
+
+}
